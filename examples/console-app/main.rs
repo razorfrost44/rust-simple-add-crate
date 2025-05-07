2c6f41d4da729d22d2ea9ad096e8c4573969ff81
Raymond Gottsch <rgottsch@logitech.com>
2025-05-07 13:00:11 -0700

    Rearrange console app into examples instead of src dir

--- examples/console-app/main.rs
+++ examples/console-app/main.rs
@@ -1,13 +1,23 @@
+// Imports - lib
 use simple_add_crate::simple_add::*;
+// Imports - examples
+pub mod apple;
+use apple::*;
 
 fn main() {
     println!("START");
     println!();
 
+    println!("Example usage from lib");
+
     let a = 7;
     let b = 9;
     let result = add_two(a, b);
     println!("The sum of {} and {} is {}", a, b, result);
+
+    println!();
+    println!("Example usage from internal extra files from examples folder");
+    print_apple();
 
     println!();
     println!("END");
